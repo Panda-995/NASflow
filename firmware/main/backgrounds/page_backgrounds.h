f8c591ebe252912ef966a9553f523221acfa9743
Panda-995 <676096193@qq.com>
2026-05-27 22:13:34 +0800

    Replace bg_page_1 with image/1.png, remove unused backgrounds and resources

--- firmware/main/backgrounds/page_backgrounds.h
+++ firmware/main/backgrounds/page_backgrounds.h
@@ -2,14 +2,4 @@
 
 #include "lvgl.h"
 
-#define PAGE_BACKGROUND_COUNT 7
-
 LV_IMG_DECLARE(bg_page_1);
-LV_IMG_DECLARE(bg_page_2);
-LV_IMG_DECLARE(bg_page_3);
-LV_IMG_DECLARE(bg_page_4);
-LV_IMG_DECLARE(bg_page_5);
-LV_IMG_DECLARE(bg_page_6);
-LV_IMG_DECLARE(bg_page_7);
-
-extern const lv_img_dsc_t *const PAGE_BACKGROUNDS[PAGE_BACKGROUND_COUNT];
